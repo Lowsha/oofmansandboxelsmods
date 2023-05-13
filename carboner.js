b571e822f8366d2d6cebb4df4716e4980cb9fd4b
oofman124 <71395038+oofman124@users.noreply.github.com>
2023-05-13 13:17:40 -0400

    Update carboner.js

--- carboner.js
+++ carboner.js
@@ -1,13 +1,4 @@
 
-elements.mayo = {
-    color: "#ffffff",
-    behavior: behaviors.LIQUID,
-    category: "liquids",
-    viscosity: 100000,
-    state: "liquid",
-    density: 720,
-};
-
 
 elements.oofman = {
     color: "#656869",
