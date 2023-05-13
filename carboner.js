8c6c38598597417b66139ab45e8792e8946ee0ba
oofman124 <71395038+oofman124@users.noreply.github.com>
2023-05-13 12:50:19 -0400

    Update carboner.js

--- carboner.js
+++ carboner.js
@@ -1,15 +1,8 @@
-elements.carboner = {
-    color: ["#f5eac6","#d4c594","#a89160","#7a5733","#523018","#361e0e"],
-    category: "life",
-    properties: {
-        dead: false,
-        
-    },
+elements.oofman = {
+    color: "#656869",
+    category: "machines",
+    state: "solid",
     tick: function(pixel) {
-       
             createPixel("carbon_dioxide", pixel.x, pixel.y+1);
-            
-        
-       
     },
 };
